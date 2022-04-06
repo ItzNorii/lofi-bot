53de5c2c39d4e777f61dfaa0dab294f1a3cd8fea
nbylaa <96096122+nbylaa@users.noreply.github.com>
2022-04-06 13:49:34 +0700

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -3,4 +3,5 @@ module.exports = {
   voice: "YOUR_DISCORD_VOICE_CHANNEL_ID",
   textchannel: "YOUR_DISCORD_TEXT_CHANNEL_ID",
   token: "YOUR_DISCORD_BOT_TOKEN"
+  // Enter your bot token in the environment, and change "YOUR_DISCORD_BOT_TOKEN" to process.env.token
 };
