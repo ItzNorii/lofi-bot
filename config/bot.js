d57dd350462bfbf0b28dfd89f5afafefa4b5ba24
nbylaa <96096122+nbylaa@users.noreply.github.com>
2022-04-06 13:04:11 +0700

    Create bot.js

---
+++ config/bot.js
@@ -0,0 +1,6 @@
+module.exports = {
+  lofi: "YOUTUBE_LOFI_VIDEO_URL",
+  voice: "YOUR_DISCORD_VOICE_CHANNEL_ID",
+  textchannel: "YOUR_DISCORD_TEXT_CHANNEL_ID",
+  token: "YOUR_DISCORD_BOT_TOKEN"
+};
